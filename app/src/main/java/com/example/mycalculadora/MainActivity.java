e53ae1e076575bc709b8f6f5b2379dd11ad35d34
Celinaldo <celinaldoalvesferre@gmail.com>
2020-10-17 19:59:14 -0300

    finaly louyt

---
+++ app/src/main/java/com/example/mycalculadora/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.mycalculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView ponto;
+    private TextView zero;
+    private ImageView limpar1;
+    private TextView igual;
+    private TextView um;
+    private TextView dois;
+    private TextView tres;
+    private TextView mais;
+    private TextView quatro;
+    private TextView cinco;
+    private TextView seis;
+    private TextView menos;
+    private TextView sete;
+    private TextView oito;
+    private TextView nove;
+    private TextView multiĺicacao;
+    private TextView limpartudo;
+    private TextView divisao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
+
